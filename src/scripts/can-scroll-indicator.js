acd65f19fe00a1067fdf72e44ee08e3dc2fd3399
drewmcarthur <drewmcarthur1@gmail.com>
2023-09-16 17:50:42 -0600

    incrementally better

--- src/scripts/can-scroll-indicator.js
+++ src/scripts/can-scroll-indicator.js
@@ -1,13 +1,27 @@
-function scrollToLinks() {
-  document.querySelector("#links").scrollIntoView({ behavior: "smooth" });
-}
+window.addEventListener("load", () => {
+  function scrollToLinks() {
+    document.querySelector("#links").scrollIntoView({ behavior: "smooth" });
+  }
 
-function hideScrollIndicator() {
-  document.querySelector("#can-scroll-indicator").style.display = "none";
-}
+  function hideScrollIndicator() {
+    document.querySelector("#can-scroll-indicator").style.display = "none";
+  }
 
-document.on("load", () => {
-  window.addEventListener("scroll", hideScrollIndicator);
+  function showScrollIndicator() {
+    document.querySelector("#can-scroll-indicator").style.display = "flex";
+  }
+
+  function manageScrollIndicator() {
+    console.log("scrolled!" + window.scroll);
+    if (window.scrollY > 20) {
+      hideScrollIndicator();
+    } else {
+      showScrollIndicator();
+    }
+  }
+
+  const app = document.querySelector("#app");
+  app.addEventListener("scroll", manageScrollIndicator);
   document
     .querySelector("#can-scroll-indicator")
     .addEventListener("click", scrollToLinks);
