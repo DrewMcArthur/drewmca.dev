7b80f23be2febebb0f07cc48ade085df8748e67c
drewmcarthur <drewmcarthur1@gmail.com>
2023-09-16 18:12:27 -0600

    no console log

--- src/scripts/can-scroll-indicator.js
+++ src/scripts/can-scroll-indicator.js
@@ -28,7 +28,6 @@ window.addEventListener("load", () => {
   }
 
   function manageScrollIndicator() {
-    console.log(app.scrollTop);
     if (app.scrollTop > 400) {
       flipScrollIndicator();
     } else {
