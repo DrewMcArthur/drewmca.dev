a9ef2ae3d417eefad603893002c95f768ec2c3ad
drewmcarthur <drewmcarthur1@gmail.com>
2025-03-25 23:54:26 -0600

    just min

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,18 +4,20 @@ import compress from "astro-compress";
 
 import purgecss from "astro-purgecss";
 
+import min from "astro-min";
+
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   minifyCSS: true,
   integrations: [
-    compress(),
-    purgecss({
-      fontFace: true,
-      keyframes: true,
-      variables: true,
-      content: ["./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}"],
-    }),
+    // compress(), purgecss({
+    // fontFace: true,
+    // keyframes: true,
+    // variables: true,
+    // content: ["./src/**/*.{astro,js,jsx,ts,tsx,vue,svelte}"],
+    // }),
+    min(),
   ],
   redirects: {
     "/bsky": "//bsky.app/profile/drewmca.dev",
