32b4a2548a8c87f1d3865346bda62e82aeff6d31
drewmcarthur <drewmcarthur1@gmail.com>
2025-03-25 23:45:11 -0600

    purge css

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from "astro/config";
 
 import compress from "astro-compress";
 
+import purgecss from "astro-purgecss";
+
 // https://astro.build/config
 export default defineConfig({
   compressHTML: true,
   minifyCSS: true,
-  integrations: [compress()],
+  integrations: [compress(), purgecss()],
   redirects: {
     "/bsky": "//bsky.app/profile/drewmca.dev",
     "/book": "//cal.com/drewmca",
